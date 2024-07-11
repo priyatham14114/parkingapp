460cae6b30e48e1d14bf7bc75cf4547ddf6e146e
priyathama pydikondala <priyatham047@gmail.com>
2024-07-11 04:00:17 +0000

    2024-07-11

--- parkingApp-CAPM/app/vendorapp/webapp/controller/VendorView.controller.js
+++ parkingApp-CAPM/app/vendorapp/webapp/controller/VendorView.controller.js
@@ -2,9 +2,12 @@ sap.ui.define([
     "./baseController",
     // "sap/ui/core/mvc/Controller",
     "sap/ui/core/Fragment",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
-    function (Controller, Fragment, MessageToast) {
+    function (Controller, Fragment, MessageToast, MessageBox, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("com.app.vendorapp.controller.VendorView", {
@@ -36,33 +39,89 @@ sap.ui.define([
                     this.oReserve = await this.loadFragment("reserveSlot")
                 }
                 this.oReserve.open()
-
-
             },
-            onBookSlotPress: function(){
-                const oModel = this.getView().getModel()
+            onBookSlotPress: function () {
+            debugger
+                const oModel = this.getView().getModel(),
+                    oUserView = this.getView(),
+                    oThis = this
                 var sDriverName = this.getView().byId("idkdjgbrrddrivername").getValue()
                 var sDriverMobile = this.getView().byId("iddrivrtmobilwInput").getValue()
                 var sVehicle = this.getView().byId("idameInput").getValue()
                 // var sTypeofdelivery = this.getView().byId("idTypeOfDelivery").getSelectedKey()
                 var sVendorName = this.getView().byId("idvendordadNameInput").getValue()
 
+
                 const newReserveModel = {
 
                     driverName: sDriverName,
-                    driverMobile:sDriverMobile,
+                    driverMobile: sDriverMobile,
                     vehicleNumber: sVehicle,
                     // deliveryType: sTypeofdelivery,
                     vendorName: sVendorName
 
                 }
-
                 const oBinding = oModel.bindList("/reservations")
-                const Reservation = oBinding.create(newReserveModel)
-                if(Reservation){
-                    MessageToast.show("Reservation Sent")
+
+                var bValid = true;
+                if (!sDriverName || sDriverName.length < 3) {
+                    oUserView.byId("idkdjgbrrddrivername").setValueState("Error");
+                    oUserView.byId("idkdjgbrrddrivername").setValueStateText("Name Must Contain 3 Characters");
+                    bValid = false;
+                } else {
+                    oUserView.byId("idkdjgbrrddrivername").setValueState("None");
+                }
+                if (!sDriverMobile || sDriverMobile.length !== 10 || !/^\d+$/.test(sDriverMobile)) {
+                    oUserView.byId("iddrivrtmobilwInput").setValueState("Error");
+                    oUserView.byId("iddrivrtmobilwInput").setValueStateText("Mobile number must be a 10-digit numeric value");
+
+                    bValid = false;
+                } else {
+                    oUserView.byId("iddrivrtmobilwInput").setValueState("None");
+                }
+                if (!sVehicle || !/^[A-Za-z]{2}\d{2}[A-Za-z]{2}\d{4}$/.test(sVehicle)) {
+                    oUserView.byId("idameInput").setValueState("Error");
+                    oUserView.byId("idameInput").setValueStateText("Vehicle number should follow this pattern AP12BG1234");
+
+                    bValid = false;
+                } else {
+                    oUserView.byId("idameInput").setValueState("None");
+                }
+                if (!sVendorName || sVendorName.length < 3) {
+                    oUserView.byId("idvendordadNameInput").setValueState("Error");
+                    oUserView.byId("idvendordadNameInput").setValueStateText("Vendor Name Must Contain 3 Characters");
+                    bValid = false;
+                } else {
+                    oUserView.byId("idvendordadNameInput").setValueState("None");
+                }
+                if (!bValid) {
+                    MessageToast.show("Please enter correct data");
+                    return; // Prevent further execution
 
                 }
+
+                var oReadReservedSlots = oModel.bindList("/reserved");
+
+                oReadReservedSlots.filter([
+                    new Filter("vehicleNumber", FilterOperator.EQ, sVehicle)
+                ]);
+
+                oReadReservedSlots.requestContexts().then(function (aReservedContext) {
+                    if (aReservedContext.length > 0) {
+                        MessageBox.warning("You can not reserve.vehicle number " + oVehicleNumber + " already reserved")
+                    } else {
+
+                        const Reservation = oBinding.create(newReserveModel)
+                        if (Reservation) {
+                            oThis.getView().byId("idkdjgbrrddrivername").setValue("")
+                            oThis.getView().byId("iddrivrtmobilwInput").setValue("")
+                            oThis.getView().byId("idameInput").setValue("")
+                            oThis.getView().byId("idvendordadNameInput").setValue("")
+                            MessageBox.success("Reservation Sent Slot allotment will be sent to your mobile number")
+
+                        }
+                    }
+                })
             }
         });
     });
